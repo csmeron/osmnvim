e60713da5b77e61cfb989e0937fd1a36cb72e062
csmeron <csmeron@proton.me>
2026-05-12 23:45:12 -0500

    netrw improvement + a comment

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,6 +1,8 @@
 local cmp = require "cmp"
 
 local options = {
+  -- Disables autocomplete when writing a comment
+
   completion = { completeopt = "menu,menuone" },
 
   window = {
@@ -23,7 +25,7 @@ local options = {
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-c>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
 
     ["<CR>"] = cmp.mapping.confirm {
